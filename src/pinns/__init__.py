cadca22ff23c91498128eda6a67c56a9f5d95db2
Baba-yara <fababa@iu.edu>
2025-06-15 12:40:53 -0400

    Add divergence operator and tests

--- src/pinns/__init__.py
+++ src/pinns/__init__.py
@@ -1,10 +1,17 @@
 from .loss import pinn_loss
-from .operators import gradient, laplacian, heat_residual, burgers_residual
+from .operators import (
+    gradient,
+    laplacian,
+    divergence,
+    heat_residual,
+    burgers_residual,
+)
 from .trainer import train_pinn
 
 __all__ = [
     "gradient",
     "laplacian",
+    "divergence",
     "heat_residual",
     "burgers_residual",
     "pinn_loss",
