d2e4a3d845c895607dfba17a4203a472f823d52a
Baba-yara <fababa@iu.edu>
2025-06-18 07:25:44 -0400

    Add Taylor series helper and notebook demo

--- src/taylor_mode/__init__.py
+++ src/taylor_mode/__init__.py
@@ -1,5 +1,5 @@
 from .jet import Jet
-from .forward import forward_derivatives, hessian
+from .forward import forward_derivatives, hessian, taylor_series_coefficients
 from .randomize import stochastic_laplacian
 from .collapse import forward_derivatives_collapsed
 
@@ -7,6 +7,7 @@ __all__ = [
     "Jet",
     "forward_derivatives",
     "hessian",
+    "taylor_series_coefficients",
     "stochastic_laplacian",
     "forward_derivatives_collapsed",
 ]
